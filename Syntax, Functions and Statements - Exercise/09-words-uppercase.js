32660653fe476fe23b4cee25d17bff6b7dac496f
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-09-19 23:36:41 +0300

    added solution

--- Syntax, Functions and Statements - Exercise/09-words-uppercase.js
+++ Syntax, Functions and Statements - Exercise/09-words-uppercase.js
@@ -1,5 +1,5 @@
 function extract(text) {
-  const words = [...text.matchAll(/\w+/g)].map((el) => el.toUpperCase());
+  const words = [...text.matchAll(/\w+/g)].map((el) => el[0].toUpperCase());
   console.log(words.join(", "));
 }
 
