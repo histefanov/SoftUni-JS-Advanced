8a85ff6d2c66463811444f0f3d57dedbfca021d3
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-09-29 14:24:03 +0300

    added solution

--- DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,5 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  document.getElementById("sum").value =
+    Number(document.getElementById("num1").value) +
+    Number(document.getElementById("num2").value);
 }
