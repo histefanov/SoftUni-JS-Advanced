d9d196e376d5543b6ecc7b765c852afb7f6d1a7b
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-05 21:53:57 +0300

    added solution

--- DOM Introduction - Exercise/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+  const text = document.getElementById("input").value;
+  const output = document.getElementById("output");
+  const sentences = text
+    .split(".")
+    .filter((sentence) => sentence)
+    .map((sentence) => sentence + ".");
+
+  for (let i = 0; i < sentences.length; i++) {
+    if (i % 3 == 0) {
+      output.appendChild(document.createElement("p"));
+    }
+    output.lastElementChild.innerHTML += sentences[i];
+  }
 }
