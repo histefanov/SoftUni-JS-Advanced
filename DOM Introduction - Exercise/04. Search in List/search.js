d70b94e8c46d1f6715314671216ff6f60c3e737c
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-05 13:41:15 +0300

    added solutions

--- DOM Introduction - Exercise/04. Search in List/search.js
+++ DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+  const towns = Array.from(document.getElementById("towns").children);
+  const searchPattern = document.getElementById("searchText").value;
+  let matches = 0;
+
+  for (const town of towns) {
+    town.style.fontWeight = "normal";
+    town.style.textDecoration = "none";
+
+    if (town.textContent.includes(searchPattern)) {
+      matches++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    }
+  }
+
+  document.getElementById("result").textContent = `${matches} matches found`;
 }
--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const searchField = document.getElementById("searchField");
 
-   }
+    const tableRows = Array.from(
+      document.querySelector("table tbody").children
+    );
+
+    for (const row of tableRows) {
+      row.classList.remove("select");
+
+      const cells = Array.from(row.children);
+
+      if (cells.some((cell) => cell.textContent.includes(searchField.value))) {
+        row.classList.add("select");
+      }
+    }
+
+    searchField.value = "";
+  }
 }
