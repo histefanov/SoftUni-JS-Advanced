78ddae801b419e6f296a87592d6f33fae24579e3
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-07 18:42:39 +0300

    added solutions

--- DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const resultBox = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', (ev) => {
+        const box = ev.target;
+        const offset = Math.floor(ev.offsetX / box.clientWidth * 100);
+        resultBox.textContent = `${offset}%`;
+        gradient.removeEventListener()
+    })
 }
