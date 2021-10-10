2cc040005addabf498a1439cda47ff1bb8fe00d2
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-10 14:14:59 +0300

    added solutions

--- DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,23 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeBtn, decodeBtn] = Array.from(document.getElementsByTagName('button'));
+
+    encodeBtn.addEventListener('click', (ev) => {
+        const inputField = ev.target.parentElement.querySelector('textarea');
+        const msg = inputField.value;
+        const encodedMsg = [...msg]
+            .map((c) => String.fromCharCode(c.charCodeAt(0) + 1))
+            .join('');
+        
+        decodeBtn.parentElement.querySelector('textarea').value = encodedMsg;
+        inputField.value = '';
+    })
+
+    decodeBtn.addEventListener('click', (ev) => {
+        const resultField = ev.target.parentElement.querySelector('textarea');
+        const decodedMsg = [...resultField.value]
+            .map((c) => String.fromCharCode(c.charCodeAt(0) - 1))
+            .join('');
+
+        resultField.value = decodedMsg;
+    })
 }
--- DOM Manipulations and Events - Exercise/06. Furniture/solution.js
+++ DOM Manipulations and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,46 @@
 function solve() {
+  const [generateBtn, buyBtn] = document.querySelectorAll('#exercise button');
+  const [generateTextarea, buyTextarea] = document.querySelectorAll('#exercise textarea');
 
-  //TODO...
+  generateBtn.addEventListener('click', (ev) => {
+    const items = JSON.parse(generateTextarea.value);
+    const table = document.getElementsByTagName('tbody')[0];
+    
+    for (const item of items) {
+      const row = document.createElement('tr');
+
+      for (let i = 0; i < 5; i++) {
+        row.appendChild(document.createElement('td'));
+      }
+
+      const thumbnail = document.createElement('img');
+      thumbnail.setAttribute('src', item.img);
+      row.children[0].appendChild(thumbnail);
+
+      const itemName = document.createElement('p');
+      itemName.textContent = item.name;
+      row.children[1].appendChild(itemName);
+
+      const price = document.createElement('p');
+      price.textContent = item.price;
+      row.children[2].appendChild(price);
+
+      const decFactor = document.createElement('p');
+      decFactor.textContent = item.decFactor;
+      row.children[3].appendChild(decFactor);
+
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      row.children[4].appendChild(checkbox);
+
+      table.appendChild(row);   
+    }
+  })
+
+  buyBtn.addEventListener('click', (ev) => {
+    const markedItems = Array.from(document.querySelectorAll('tbody tr'))
+      .filter((r) => r.lastElementChild.firstChild.checked == true);
+
+    console.log(markedItems);
+  })
 }
