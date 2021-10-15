c1de21a9cd2c7567e8b02c1bec09f218b1aaea8f
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-15 09:03:55 +0300

    added solution

--- Advanced Functions - Exercise/08. Next Article/script.js
+++ Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const articlesCopy = [...articles];
+
+    return showNext;
+
+    function showNext() {
+        const next = articlesCopy.shift();
+
+        if (next) {
+            const article = document.createElement('article');
+            article.textContent = next;
+
+            document.getElementById('content').appendChild(article);
+        }
+    }
 }
