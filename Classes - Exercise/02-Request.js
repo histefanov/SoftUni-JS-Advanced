9f179536c810c658f99bb8830589bb991b319681
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-20 14:47:29 +0300

    added a couple of classes exercises

---
+++ Classes - Exercise/02-Request.js
@@ -0,0 +1,10 @@
+class Request {
+    constructor(method, uri, version, message) {
+        Object.assign(this, {
+            method, uri, version, message
+        })
+
+        this.response = undefined;
+        this.fulfilled = false;
+    }
+}
